668d111dd779aa2cca966896909f1df32102fc95
Takeru  Okamura <takeru.okamura@M010559353.local>
2026-04-27 16:50:37 +0900

    lsコマンドの-aオプション機能追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,21 +1,41 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+require 'optparse'
 
 COLS = 3
 
-items = Dir.glob('*').sort
-rows = (items.size.to_f / COLS).ceil
+#引数を受け取る　-aだけ受け付ける
+command = ARGV[0].delete('-') # -aの-を削除してaだけにする
 
-grid = Array.new(rows) { Array.new(COLS) }
-items.each_with_index do |name, i|
-  row = i % rows
-  col = i / rows
-  grid[row][col] = name
+# コマンドが複数あれば、for文で回す
+items = case command
+when 'a'
+  Dir.glob("*", File::FNM_DOTMATCH).sort
+else
+  Dir.glob('*').sort
 end
 
-width = items.map(&:length).max + 2
 
-grid.each do |row|
-  row.each { |name| printf "%-#{width}s", (name || '') }
-  puts
+def print_items(items)
+  # 3列で表示するための行数を計算
+  rows = (items.size.to_f / COLS).ceil
+
+  # アイテムを行列に配置
+  grid = Array.new(rows) { Array.new(COLS) }
+  items.each_with_index do |name, i|
+    row = i % rows
+    col = i / rows
+    grid[row][col] = name
+  end
+
+  # 列の幅を計算
+  width = items.map(&:length).max + 2
+
+  # 行列を表示
+  grid.each do |row|
+    row.each { |name| printf "%-#{width}s", (name || '') }
+    puts
+  end
 end
+
+print_items(items)
