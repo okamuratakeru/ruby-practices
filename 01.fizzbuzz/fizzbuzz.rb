974591ef6fa237020c80ede588892217b3115aae
Takeru  Okamura <takeru.okamura@M010559353.local>
2026-04-28 13:31:27 +0900

    なしにするためにputsに変更

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
 (1..20).each do |i|
   if i % 5 == 0 && i % 3 == 0
-    p "FizzBuzz"
+    puts "FizzBuzz"
   elsif i % 5 == 0
-    p "Buzz"
+    puts "Buzz"
   elsif i % 3 == 0
-    p "Fizz"
+    puts "Fizz"
   else
-    p i
+    puts i
   end
 end
