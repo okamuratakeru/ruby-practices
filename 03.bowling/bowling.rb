4b858d7ee23a3e6892ac1ddc38b6ecf0b01aaef7
okamuratakeru <takeokamura11@gmail.com>
2026-06-17 12:40:15 +0900

    レビュー修正/結果が変わらないため...　→ 　..に修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -14,7 +14,7 @@ end
 
 # スコアを計算する
 score = frame_starts.sum do |shot_index|
-  end_offset = if shots[shot_index] == 10 || shots[shot_index...(shot_index + 2)].sum == 10
+  end_offset = if shots[shot_index] == 10 || shots[shot_index..(shot_index + 1)].sum == 10
                  2
                else
                  1
